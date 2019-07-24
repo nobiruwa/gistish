b80f8fd431d099ab599b63e06e14ed7db702ae42
ein <ein@hpnull.localdomain>
2019-07-24 09:23:57 +0900

    activiti-example: learn how to implement activiti's java service task.

---
+++ activiti-example/src/main/java/com/example/MyFirstServiceTask.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class MyFirstServiceTask implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+        
+    }
+}
