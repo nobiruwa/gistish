2857851a50f4d0f05469f32ff3491702e63a0080
ein <ein@hpnull.localdomain>
2019-12-04 00:37:57 +0900

    add drools-example

---
+++ drools-example/README.md
@@ -0,0 +1,3 @@
+# drools-example
+
+- <https://docs.jboss.org/drools/release/7.30.0.Final/drools-docs/html_single/index.html#decision-tables-defining-proc>
---
+++ drools-example/src/main/java/com/example/drools/Order.java
@@ -0,0 +1,13 @@
+package com.example.drools;
+
+public class Order {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
