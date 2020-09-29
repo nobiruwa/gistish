38d1552aa9f0e59a4eab2d29a3e2a49a42cb0d00
ein <ein@hpnull.localdomain>
2020-09-29 17:05:25 +0900

    http-server: update

--- http-server/app/Main.hs
+++ http-server/app/Main.hs
@@ -1,46 +1,56 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad (when)
 import Data.Maybe (isJust)
 import Network.Wai.Application.Static (StaticSettings(..), defaultFileServerSettings, staticApp)
 import Network.Wai.Handler.Warp (Port, Settings, defaultSettings, getHost, getPort, runSettings, setPort)
 import Network.Wai.Handler.WarpTLS (runTLS, tlsSettings)
 import Network.Wai.Middleware.Cors (simpleCors)
-import System.Console.GetOpt (ArgDescr(..), ArgOrder(..), OptDescr(..), getOpt)
+import System.Console.GetOpt (ArgDescr(..), ArgOrder(..), OptDescr(..), getOpt, usageInfo)
 import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 
 -- 引数のデータ構造
-data Options = Options { optPort :: Port
-                       , optDirectory :: FilePath
-                       , optCors :: Bool
-                       , optCert  :: Maybe FilePath
-                       , optKey :: Maybe FilePath
-                       }
+data Options = Options
+  { optPort :: Port
+  , optDirectory :: FilePath
+  , optCors :: Bool
+  , optCert  :: Maybe FilePath
+  , optKey :: Maybe FilePath
+  , optHelp :: Bool
+  } deriving (Eq, Show)
 
-defaultOptions = Options { optPort = 3000
-                         , optDirectory = "."
-                         , optCors = True
-                         , optCert = Nothing
-                         , optKey = Nothing
-                         }
+defaultOptions = Options
+  { optPort = 3000
+  , optDirectory = "."
+  , optCors = True
+  , optCert = Nothing
+  , optKey = Nothing
+  , optHelp = False
+  }
 
 options :: [OptDescr (Options -> Options)]
-options = [ Option ['p'] ["port"]
-            (ReqArg (\f opts -> opts { optPort = read f }) "PORT")
-            "PORT number"
-          , Option ['d'] ["directory"]
-            (ReqArg (\f opts -> opts { optDirectory = f }) "DIR")
-            "root DIR"
-          , Option [] ["disable-cors"]
-            (NoArg (\opts -> opts { optCors = False }))
-            "disable cors"
-          , Option ['c'] ["certificate"]
-            (ReqArg (\f opts -> opts { optCert = Just f }) "CERT")
-            "certificate file"
-          , Option ['k'] ["key"]
-            (ReqArg (\f opts -> opts { optKey = Just f }) "KEY")
-            "key file"
-          ]
+options =
+  [ Option ['p'] ["port"]
+    (ReqArg (\f opts -> opts { optPort = read f }) "PORT")
+    "PORT number"
+  , Option ['d'] ["directory"]
+    (ReqArg (\f opts -> opts { optDirectory = f }) "DIR")
+    "root DIR"
+  , Option [] ["disable-cors"]
+    (NoArg (\opts -> opts { optCors = False }))
+    "disable cors"
+  , Option ['c'] ["certificate"]
+    (ReqArg (\f opts -> opts { optCert = Just f }) "CERT")
+    "certificate file"
+  , Option ['k'] ["key"]
+    (ReqArg (\f opts -> opts { optKey = Just f }) "KEY")
+    "key file"
+  , Option ['?', 'h'] ["help"]
+    (NoArg (\opts -> opts { optHelp = True }))
+    "show help message"
+  ]
 
 compilerOpts :: [String] -> IO (Options, [String])
 compilerOpts args =
@@ -82,12 +92,19 @@ instance Show StaticSettings where
     " }"
 
 tlsOptions :: Options -> Maybe (FilePath, FilePath)
-tlsOptions (Options _ _ _ (Just cert) (Just key)) = Just (cert, key)
+tlsOptions (Options _ _ _ (Just cert) (Just key) _) = Just (cert, key)
 tlsOptions _ = Nothing
 
 main :: IO ()
 main = do
-  (opts, n) <- getArgs >>= compilerOpts
+  (o, n) <- getArgs >>= compilerOpts
+  -- non-optionsの引数を--directoryオプションよりも優先する
+  let opts = if length n > 0
+        then o { optDirectory = head n }
+        else o
+  when (optHelp opts) $ do
+    putStrLn (usageInfo "http-server-exe [OPTIONS] [DIRECTORY]" options)
+    exitSuccess
   let s = settings $ optPort opts
   let ss = staticSettings $ optDirectory opts
   let application = if optCors opts
