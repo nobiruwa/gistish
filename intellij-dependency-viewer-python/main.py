ad25ca17ddd11788b70c08d8cb1cf8669db342c1
ein <ein@hpnull.localdomain>
2021-05-19 09:16:30 +0900

    intellij-dependency-viewer-python: add '--reverse' option.

--- intellij-dependency-viewer-python/main.py
+++ intellij-dependency-viewer-python/main.py
@@ -10,18 +10,25 @@ import writer
 
 import sys
 
-def main(input_path, output_path, root=None):
+
+def create_dependency(x, reverse):
+    if reverse:
+        return core.Dependency(
+            transformer.path.path_to_fqdn(x.to_),
+            transformer.path.path_to_fqdn(x.from_),
+        )
+    else:
+        return core.Dependency(
+            transformer.path.path_to_fqdn(x.from_),
+            transformer.path.path_to_fqdn(x.to_)
+        )
+
+def main(input_path, output_path, root=None, reverse=False):
     r = reader.IntelliJXmlReader()
     dependencies = r.from_file(input_path)
 
     transformed = [
-        core.Dependency(
-            # transformer.path.path_to_fqdn(x.from_),
-            # transformer.path.path_to_fqdn(x.to_)
-            transformer.path.path_to_class_name(x.from_),
-            transformer.path.path_to_class_name(x.to_),
-            x
-        )
+        create_dependency(x, reverse)
         for x in filter_.remove_if_to_is_in_jar(dependencies)
     ]
 
@@ -38,13 +45,22 @@ def main(input_path, output_path, root=None):
 
 
 if __name__ == '__main__':
+    # 要素の削除を行うためのコピー配列を作成する
+    args = sys.argv.copy()
 
-    if len(sys.argv) >= 5:
-        # main('samples/two_tree.xml', 'output/two_tree.dot', 'Root2', 'ManyChildrenClass')
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.arg[4])
-    elif len(sys.argv) >= 4:
-        # main('samples/two_tree.xml', 'output/two_tree.dot', 'Root2')
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    # 簡易な引数のパースを行う
+    if '--reverse' in args:
+        reverse = True
+        args.remove('--reverse')
     else:
-        # main('samples/two_tree.xml', 'output/two_tree.dot')
-        main(sys.argv[1], sys.argv[2])
+        reverse = False
+
+    # 残った引数の数で動作を分岐する
+    if len(args) >= 4:
+        # python3 main.py samples/two_tree.xml /tmp/two_tree.dot org.example.layer1.Root2
+        # python3 main.py samples/two_tree.xml /tmp/two_tree_reverse.dot org.example.layer1.Root2 --reverse
+        main(args[1], args[2], root=args[3], reverse=reverse)
+    else:
+        # python3 main.py samples/two_tree.xml /tmp/two_tree.dot
+        # python3 main.py samples/two_tree.xml /tmp/two_tree_reverse.dot --reverse
+        main(args[1], args[2], root=None, reverse=reverse)
