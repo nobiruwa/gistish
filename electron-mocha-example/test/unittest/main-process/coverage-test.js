17d20b1cb27cd5cc51d8e5fd94fdaa101d16b136
ein <ein@mousenull>
2022-03-30 14:28:04 +0900

    electron-mocha-example: updated dependencies.

--- electron-mocha-example/test/unittest/main-process/coverage-test.js
+++ electron-mocha-example/test/unittest/main-process/coverage-test.js
@@ -1,13 +1,16 @@
 /*global __coverage__:false, __dirname:false, global:false*/
 import { resolve, join } from 'path';
 import process from 'process';
-import { writeFileSync as write } from 'fs';
+import { mkdirSync as mkdir, statSync as stat, writeFileSync as write } from 'fs';
 
 const root = resolve(__dirname, '..', '..', '..');
 const tmpd = resolve(root, '.nyc_output');
 
 function report() {
   if (global.__coverage__) {
+    if (!stat(tmpd).isDirectory()) {
+      mkdir(tmpd);
+    }
     write(join(tmpd, `${process.type}.json`), JSON.stringify(global.__coverage__));
   }
 }
