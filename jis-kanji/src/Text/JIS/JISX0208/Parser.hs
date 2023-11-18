8aa7b8b25aacf22ec2ec213b96b42edcb7ae8197
ein <ein@mousenull>
2023-11-18 21:37:03 +0900

    haskell projects' resolver updated.

--- jis-kanji/src/Text/JIS/JISX0208/Parser.hs
+++ jis-kanji/src/Text/JIS/JISX0208/Parser.hs
@@ -1,39 +1,39 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 module Text.JIS.JISX0208.Parser where
 
-import Text.JIS.JISX0208.JISChar (JISChar, newJISChar)
-
 import Data.Maybe (catMaybes)
-import Text.Parsec ((<|>), ParseError, ParsecT, Stream, char, endOfLine, hexDigit, many, parse, satisfy, sepEndBy, space, string, tab, try)
+import Text.JIS.JISX0208.JISChar (JISChar, newJISChar)
+import Text.Parsec (ParseError, ParsecT, char, endOfLine, hexDigit, many, parse, satisfy, sepEndBy, space, string, tab, try, (<|>))
 
 parseFile :: String -> Either ParseError [JISChar]
 parseFile content = catMaybes <$> parse parseContent "JISX0208.TXT" content
 
-parseContent :: Stream String m t => ParsecT String () m [Maybe JISChar]
+parseContent :: (Monad m) => ParsecT String () m [Maybe JISChar]
 parseContent = parseLine `sepEndBy` endOfLine
 
-parseLine :: Stream String m t => ParsecT String () m (Maybe JISChar)
+parseLine :: (Monad m) => ParsecT String () m (Maybe JISChar)
 parseLine = try commentLine <|> charLine
 
-commentLine :: Stream String m t => ParsecT String () m (Maybe JISChar)
+commentLine :: (Monad m) => ParsecT String () m (Maybe JISChar)
 commentLine =
   Nothing
-  <$ char '#'
-  <* many (satisfy (/= '\n'))
+    <$ char '#'
+    <* many (satisfy (/= '\n'))
 
-charLine :: Stream String m t => ParsecT String () m (Maybe JISChar)
+charLine :: (Monad m) => ParsecT String () m (Maybe JISChar)
 charLine =
   (Just <$>)
-  $ newJISChar
-  <$> readHex
-  <* tab
-  <*> readHex
-  <* tab
-  <*> readHex
-  <* (tab >> char '#' >> space)
-  <*> many (satisfy (/= '\n'))
+    $ newJISChar
+    <$> readHex
+    <* tab
+    <*> readHex
+    <* tab
+    <*> readHex
+    <* (tab >> char '#' >> space)
+    <*> many (satisfy (/= '\n'))
 
-readHex :: Stream String m t => ParsecT String () m String
+readHex :: (Monad m) => ParsecT String () m String
 readHex =
   string "0x"
-  *> many hexDigit
+    *> many hexDigit
