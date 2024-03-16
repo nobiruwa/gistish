ed7b3cbce076b266c9ce6c0f436005bcce0851b5
ein <ein@mousenull>
2024-03-16 14:45:07 +0900

    updated resolvers.

--- geosearch/src/Web/Location.hs
+++ geosearch/src/Web/Location.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Web.Location (locationHandlers) where
 
-import Data.Location (Geometric(..))
-import Data.Location.Repository (findWithinAndLimit, findByPrefectureNameAndCityName, findByPrefectureName, findById, findByPrefectureNameAndCityNameAndTownNameContaining)
+module Web.Location (locationHandlers) where
 
 import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
+import Data.Location (Geometric (..))
+import Data.Location.Repository (findById, findByPrefectureName, findByPrefectureNameAndCityName, findByPrefectureNameAndCityNameAndTownNameContaining, findWithinAndLimit)
 import Data.Map (fromList)
 import qualified Data.Map as Map
 import Database.HDBC (withTransaction)
 import Database.HDBC.PostgreSQL (Connection)
 import Network.HTTP.Types.Status (status404)
-import Web.Scotty (ActionM, params, get, json, status, param, ScottyM)
+import Web.Scotty (ActionM, ScottyM, captureParam, get, json, queryParam, queryParams, status)
 
 locationHandlers :: Connection -> ScottyM ()
 locationHandlers conn = do
@@ -21,7 +21,7 @@ locationHandlers conn = do
 
 getLocationById :: Connection -> ScottyM ()
 getLocationById conn = get "/location/:id" $ do
-  id_ <- param "id"
+  id_ <- captureParam "id"
   mLocation <- liftIO . withTransaction conn $ flip findById id_
   case mLocation of
     Nothing -> status status404
@@ -29,21 +29,21 @@ getLocationById conn = get "/location/:id" $ do
 
 getLocationSearch :: Connection -> ScottyM ()
 getLocationSearch conn = get "/location" $ do
-  prefectureName <- param "prefecture"
-  ps <- fromList <$> params
+  prefectureName <- queryParam "prefecture"
+  ps <- fromList <$> queryParams
   locs <- liftIO $ case Map.lookup "city" ps of
     Nothing -> withTransaction conn $ flip findByPrefectureName prefectureName
     (Just cityName) -> case Map.lookup "town" ps of
       Nothing -> findByPrefectureNameAndCityName conn prefectureName cityName
       (Just townName) -> findByPrefectureNameAndCityNameAndTownNameContaining conn prefectureName cityName townName
-  when (length locs == 0) (status status404)
+  when (null locs) (status status404)
   json locs
 
 getDistance :: Connection -> ScottyM ()
 getDistance conn = get "/distance" $ do
-  lon <- param "lon" :: ActionM Double
-  lat <- param "lat" :: ActionM Double
-  within <- param "within"
-  limit <- param "limit"
+  lon <- queryParam "lon" :: ActionM Double
+  lat <- queryParam "lat" :: ActionM Double
+  within <- queryParam "within"
+  limit <- queryParam "limit"
   locs <- liftIO $ findWithinAndLimit conn (Geometric lon lat) within limit
   json locs
