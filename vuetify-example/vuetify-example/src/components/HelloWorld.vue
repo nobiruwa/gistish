2339b97529cbdea72c7e37e73243bcd49480005f
ein <ein@mousenull>
2025-08-12 23:16:44 +0900

    updated.

--- vuetify-example/vuetify-example/src/components/HelloWorld.vue
+++ vuetify-example/vuetify-example/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="logoSrc"
           class="my-3"
           contain
           height="200"
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
+          Welcome to the Vuetify 3
         </h1>
 
 
@@ -97,6 +97,11 @@
 export default {
   name: 'HelloWorld',
 
+  computed: {
+    logoSrc() {
+      return './assets/logo.svg';
+    },
+  },
   data: () => ({
     ecosystem: [
       {
