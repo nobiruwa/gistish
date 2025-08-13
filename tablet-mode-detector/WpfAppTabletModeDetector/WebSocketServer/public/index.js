4d0cef2161b31ea655e86720843dc363aa59ddfb
ein <ein@mousenull>
2025-08-13 22:11:25 +0900

    updated.

--- tablet-mode-detector/WpfAppTabletModeDetector/WebSocketServer/public/index.js
+++ tablet-mode-detector/WpfAppTabletModeDetector/WebSocketServer/public/index.js
@@ -1,14 +1,25 @@
 window.addEventListener("load", function () {
-    var ws = new WebSocket("ws://" + window.location.hostname + ":3001/chat");
+  var ws = new WebSocket("ws://" + window.location.hostname + ":3001/chat");
 
-    ws.onmessage = function(e) {
-        if (e.data) {
-            var json = JSON.parse(e.data);
-            var tabletModeText = document.getElementById("tablet-mode");
-            tabletModeText.innerHTML = json.message;
-        }
+  ws.onmessage = function(e) {
+    const fileReader = new FileReader();
+
+    fileReader.onload = (e) => {
+      const json = JSON.parse(fileReader.result);
+      const tabletModeText = document.getElementById("tablet-mode");
+      tabletModeText.innerHTML = json.message;
     };
-    ws.onopen = function(e) {
-        ws.send(JSON.stringify({ message: "current-mode" }));
-    };
+    if (e.data) {
+      if (e.data instanceof Blob) {
+        fileReader.readAsText(e.data);
+      } else {
+        const json = JSON.parse(e.data);
+        const tabletModeText = document.getElementById("tablet-mode");
+        tabletModeText.innerHTML = json.message;
+      }
+    }
+  };
+  ws.onopen = function(e) {
+    ws.send(JSON.stringify({ message: "current-mode" }));
+  };
 });
