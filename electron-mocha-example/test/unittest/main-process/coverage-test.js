0bf8dac4a8895ce1162d0d161f58c99a2c59a3a4
ein <ein@mousenull>
2025-08-13 17:04:04 +0900

    updated.

--- electron-mocha-example/test/unittest/main-process/coverage-test.js
+++ electron-mocha-example/test/unittest/main-process/coverage-test.js
@@ -1,14 +1,14 @@
 /*global __coverage__:false, __dirname:false, global:false*/
 import { resolve, join } from 'path';
 import process from 'process';
-import { mkdirSync as mkdir, statSync as stat, writeFileSync as write } from 'fs';
+import { existsSync as exists, mkdirSync as mkdir, lstatSync as lstat, writeFileSync as write } from 'fs';
 
 const root = resolve(__dirname, '..', '..', '..');
 const tmpd = resolve(root, '.nyc_output');
 
 function report() {
   if (global.__coverage__) {
-    if (!stat(tmpd).isDirectory()) {
+    if (!exists(tmpd) || !lstat(tmpd).isDirectory()) {
       mkdir(tmpd);
     }
     write(join(tmpd, `${process.type}.json`), JSON.stringify(global.__coverage__));
